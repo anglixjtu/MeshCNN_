ad54a496971eaadfa58760dea1b927543d1caadf
Ang Li@vaio <angli522@foxmail.com>
2021-09-07 08:25:24 +0800

    fix: add load data/model options

---
+++ src/options/load_options.py
@@ -0,0 +1,18 @@
+def load_model_opt(path, opt=None):
+    if not opt:
+        opt = type('Opt', (object,), {})()
+    load_list = {'arch': str, 'fc_n': int, 'input_nc': int,
+                 'mode': str, 'ncf': list, 'neigbs': int,
+                 'ninput_edges': int, 'nclasses': int}
+    with open(path) as f:
+        lines = f.readlines()
+        for line in lines[1:-1]:
+            name, value = line.strip('\n').split(': ')
+            if name in load_list.keys():
+                if load_list[name] == list:
+                    value = value.strip('[] ').split(',')
+                    setattr(opt, name, [int(x) for x in value])
+                else:
+                    value = load_list[name](value)
+                    setattr(opt, name, value)
+    return opt
