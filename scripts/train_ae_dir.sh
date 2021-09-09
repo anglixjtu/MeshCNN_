93456318d914d0fe88c790b179fdb3c213952fea
Ang Li@vaio <angli522@foxmail.com>
2021-09-09 08:31:02 +0800

    fix: add augmentation 1

--- scripts/train_ae_dir.sh
+++ scripts/train_ae_dir.sh
@@ -3,18 +3,17 @@
 ## training the autoencoder network
 python train.py \
 --dataroot data/mcbb_tiny \
---name fix_tiny_ae_9 \
+--name fix_tiny_ae8_aug1 \
 --ncf 32 64 64 128 512 \
 --mode autoencoder \
 --flip_edges 0.2 \
 --slide_verts 0.2 \
 --num_aug 20 \
 --niter_decay 100 \
---input_nc 9 \
+--input_nc 8 \
 --neigbs 11 \
 --batch_size 2 \
 --num_threads 4 \
 --gpu_ids -1 \
 --arch mesh_aec \
 --loss chamfer \
---len_feature True \
--- src/data/transform_recipes.py
+++ src/data/transform_recipes.py
@@ -7,6 +7,39 @@ from torch_geometric.transforms import (NormalizeScale,
 
 
 def set_transforms(phase, opt, mean=0, std=1, ninput_channels=5):
+    
+    if phase in 'train':
+        graph_transformer = \
+            [ConstructEdgeGraph(ninput_edges=opt.ninput_edges,
+                                num_aug=opt.num_aug,
+                                neigbs=opt.neigbs,
+                                scale_verts=opt.scale_verts,
+                                flip_edges=opt.flip_edges,
+                                slide_verts=opt.slide_verts,
+                                len_feature=opt.len_feature)]
+        general_transformer = [NormalizeScale(),
+                               CatPos(opt.input_nc)]
+    else:  # no augmentation for test and retrieval
+        graph_transformer = \
+            [ConstructEdgeGraph(ninput_edges=opt.ninput_edges,
+                                num_aug=1,
+                                neigbs=opt.neigbs,
+                                len_feature=opt.len_feature)]
+        general_transformer = [NormalizeScale(),
+                               NormalizeRotation(),
+                               CatPos(opt.input_nc)]
+
+    if phase in ['compute_mean_std']:
+        general_transformer = [NormalizeScale(),
+                               CatPos(opt.input_nc)]
+        return Compose(graph_transformer + general_transformer)
+    else:
+        return Compose(graph_transformer +
+                       general_transformer +
+                       [NormalizeFeature(mean, std, ninput_channels)])
+
+
+def set_transforms0(phase, opt, mean=0, std=1, ninput_channels=5):
     general_transformer = [NormalizeScale(),
                            NormalizeRotation(),
                            CatPos(opt.input_nc)]
