11cf84fac35821fdcffa21ac0069afc01228b85d
Ang Li@vaio <angli522@foxmail.com>
2021-09-09 09:42:17 +0800

    feat: add show_namelist.py

---
+++ tests/show_namelist.py
@@ -0,0 +1,6 @@
+def show_namelist():
+    return ['test/gear/00070120.obj', 'test/gear/00000219.obj',
+            'test/motor/00053644.obj', 'test/motor/00064756.obj',
+            'test/pin/00052497.obj', 'test/pin/00052388.obj',
+            'test/rotor/00067691.obj', 'test/rotor/00067746.obj',
+            'test/washer/00040300.obj', 'test/washer/00041575.obj']
