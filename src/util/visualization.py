06126593beff156b975f385066eb47267648764c
Ang Li@vaio <angli522@foxmail.com>
2021-09-29 11:02:37 +0800

    fix: visualization

--- src/util/visualization.py
+++ src/util/visualization.py
@@ -6,26 +6,31 @@ import time
 from torch_geometric.nn import global_mean_pool, global_add_pool, global_max_pool, global_sort_pool
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
-import json
-import math
-import torch
 from .util import get_labels_from_path
 
 
 def add_subplot(plotter, coord_y, coord_x,
-                label,  mesh, font_size, dissm=None):
+                mesh, font_size, label=None,
+                dissm=None, filename=None,
+                show_edges=True):
     plotter.subplot(coord_y, coord_x)
-    plotter.add_text("{}".format(label),
-                     font_size=font_size, color='black')
+
+    text = ''
+    if label is not None:
+        text += label + '\n'
     if dissm is not None:
-        plotter.add_text("\n\ndistance: %.3f" %
-                         (dissm), font_size=font_size, color='black')
+        text += "distance: %.3f \n" % (dissm)
+    if filename is not None:
+        text += filename
+    if label or dissm or filename:
+        plotter.add_text(text, font_size=font_size, color='black')
     plotter.set_background('white')
-    plotter.add_mesh(mesh, color="tan", show_edges=True)
+    plotter.add_mesh(mesh, color="tan", show_edges=show_edges)
 
 
 def visualize_retrieval(paths_q, paths_retr, dissm=None, show_self=False,
-                        sub_size=(220, 150), font_size=10, out_path=None):
+                        sub_size=(220, 150), font_size=10, out_path=None,
+                        camera_pos=[4, 4, 4]):
     num_query = len(paths_q)
     if show_self:
         start_ri = 0
@@ -39,23 +44,28 @@ def visualize_retrieval(paths_q, paths_retr, dissm=None, show_self=False,
                    window_size=fig_size, border_color='gray')
     for qi, path_q in enumerate(paths_q):
         mesh_q = pv.read(path_q)
+        _, filename = os.path.split(path_q)
         label = get_labels_from_path(path_q)
         label = 'Query - ' + label
-        add_subplot(p, qi, 0, label, mesh_q, font_size)
+        add_subplot(p, qi, 0, mesh_q, font_size,
+                    label=label, filename=filename)
+        p.set_position(camera_pos)
 
         for ri, path_r in enumerate(paths_retr[qi][start_ri:]):
             mesh_r = pv.read(path_r)
+            _, filename = os.path.split(path_r)
             label = get_labels_from_path(path_r)
             dissm_r = dissm[qi, ri+start_ri]
-            add_subplot(p, qi, ri+1, label, mesh_r, font_size, dissm_r)
+            add_subplot(p, qi, ri+1, mesh_r,
+                        font_size, dissm=dissm_r,
+                        label=label, filename=filename)
+            p.set_position(camera_pos)
     p.show(screenshot=out_path)
-
-
 
 
 def show_embedding(self, features, idx_list):
     label_list = self.get_labels_from_index(idx_list)
     writer = SummaryWriter('runs/embedding')
     writer.add_embedding(features,
-                             metadata=label_list)
+                         metadata=label_list)
     writer.close()
