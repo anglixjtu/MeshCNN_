9d1edebd5dbb329aea593a4422be8699b9af1bab
Ang Li@vaio <angli522@foxmail.com>
2021-09-30 14:30:04 +0800

    feat: setting for remove positions

--- scripts/train_ae_dir.sh
+++ scripts/train_ae_dir.sh
@@ -3,11 +3,11 @@
 ## training the autoencoder network
 python train.py \
 --dataroot data/mcbb_tiny \
---name fix_tiny_ae8_aug4 \
+--name fix_tiny_ae5_aug4 \
 --mode autoencoder_pt \
 --niter_decay 100 \
 --niter 100 \
---input_nc 8 \
+--input_nc 5 \
 --batch_size 2 \
 --num_threads 4 \
 --gpu_ids -1 \
